b1a04b8c3f2b28083cc1996c149931637aee449b
janascher <janainascher@protonmail.com>
2023-08-11 19:28:12 -0300

    feat: add class_07

---
+++ app/api/__init__.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+    from api.server import app
+     # Iniciar o servidor FastAPI
+    app.run()
---
+++ app/api/configs/__init__.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+# Carregar as variáveis de ambiente do arquivo .env
+load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+
+# Definir as variáveis de ambiente
+HOST = os.getenv('HOST')
+PORT = int(os.getenv('PORT'))  # Converte a variável PORT para um número inteiro
